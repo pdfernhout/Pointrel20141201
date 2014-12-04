696a37ae13f832ad48c3b9c75d4b627ed11c5edd
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-12-04 08:04:29 -0500

    Added loadResourcesForTag convenience method to client

--- client/pointrel20141201Client.js
+++ client/pointrel20141201Client.js
@@ -1,14 +1,18 @@
+"use strict";
+    
 define([
+    "dojo/promise/all",
+    "dojo/Deferred",
     "dojo/request/xhr",
     'dojox/encoding/digests/_base',
     "dojox/encoding/digests/SHA256"
 ], function(
+    all,
+    Deferred,
     xhr,
     digests,
     SHA256
 ) {
-    "use strict";
-    
     var apiPath = "/api/pointrel20141201/";
     var resourcesPath = apiPath + "resources/";
     var idIndexPath = apiPath + "indexes/id/";
@@ -42,15 +46,14 @@ define([
             callback(null, JSON.parse(data));
         }, function(error) {
             // Error
-            console.log("pointrel_storeInNewEnvelope error", error);
             if (error.response.status === 409) {
                 console.log("pointrel_storeInNewEnvelope already exists", error);
                 console.log("pointrel_storeInNewEnvelope message: '%s'", error.response.data);
                 // Assuming it's OK if the resource already exists -- we can assume it is identical and was added by someone else
                 callback(null, JSON.parse(error.response.data));
             } else {
-                console.log("pointrel_storeInNewEnvelope error not 409", error);
-                callback(error, null);
+                console.log("pointrel_storeInNewEnvelope error", error);
+                callback(error.response.data, null);
             }
         }, function(event) {
             // Handle a progress event from the request if the browser supports XHR2
@@ -108,6 +111,48 @@ define([
         });
     }
     
+    /* Convenience */
+    
+    function fetchItem(resourceToContentMap, sha256AndLength) {
+        console.log("fetchItem", sha256AndLength);
+        var deferred = new Deferred();
+        pointrel_fetchEnvelope(sha256AndLength, function(error, envelope) {
+            if (error) {
+                console.log("error", error);
+                // TODO: Could "reject" here to generate an error, but then anyone failure could stop loading others
+                // TODO: Could return the sha256AndLength of the failed item instead? Then would need to check map to see if it is there
+                deferred.resolve(sha256AndLength);
+                return;
+            }
+            console.log("Got item", envelope.content);
+            resourceToContentMap[sha256AndLength] = envelope.content;
+            deferred.resolve(sha256AndLength);
+        });
+        return deferred.promise;
+    }
+    
+    function loadResourcesForTag(resourceToContentMap, tag, callback) {
+        console.log("loadAllResourcesForTag");
+        pointrel_queryByTag(tag, function(error, queryResult) {
+            if (error) { console.log("loadResourcesForTag error", error); return callback(error);}
+            console.log("Got queryResult for tag", tag, queryResult);
+            
+            var items = queryResult.items;
+            
+            var promises = [];
+            for (var index in items) {
+                var sha256AndLength = items[index];
+                if (!resourceToContentMap[sha256AndLength]) {
+                    promises.push(fetchItem(resourceToContentMap, sha256AndLength));
+                }
+            }
+            
+            all(promises).then(function(newItems) {
+                callback(null, resourceToContentMap, newItems);
+            });            
+        });
+    }
+    
     // Optional initialization
     function initialize(configuration) {
         if (configuration) {
@@ -122,6 +167,7 @@ define([
         storeInNewEnvelope: pointrel_storeInNewEnvelope,
         fetchEnvelope: pointrel_fetchEnvelope,
         queryByID: pointrel_queryByID,
-        queryByTag: pointrel_queryByTag
+        queryByTag: pointrel_queryByTag,
+        loadResourcesForTag: loadResourcesForTag
     };
 });
