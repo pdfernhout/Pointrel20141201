0a3aae8f2fe57187d2c14e8f63330b24765fb6fa
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-02-17 11:06:08 -0500

    Changed test server to run on port 8000; changing the path to the client library it returns for previous move of that file

--- server/pointrel20141201ServerTest.js
+++ server/pointrel20141201ServerTest.js
@@ -21,13 +21,13 @@ app.use("/$",   function(req, res) {
 */
 
 // For testing only...
-app.use("/test/pointrel20141201Client.js", express.static(__dirname + "/../client/pointrel20141201Client.js"));
+app.use("/test/pointrel20141201Client.js", express.static(__dirname + "/../WebContent/js/pointrel20141201Client.js"));
 app.use("/test", express.static(__dirname + "/../test"));
 
 pointrel20141201Server.initialize(app);
 
 // Create an HTTP service.
-var server = http.createServer(app).listen(8080, function () {
+var server = http.createServer(app).listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Pointrel20141201 server app listening at http://%s:%s', host, port);
