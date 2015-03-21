e772422eae1ec13664291d42453a920cbe6ac5b0
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-21 08:31:43 -0400

    Store test resource in documentCollectionTest

--- WebContent/testing/documentCollectionTest.js
+++ WebContent/testing/documentCollectionTest.js
@@ -1,20 +1,48 @@
 require([
+    "js/pointrel20141201Client",
     "js/pointrel20141201DocumentCollection",
     "dojo/domReady!"
 ], function(
+    pointrel20141201Client,
     DocumentCollection
 ){
     "use strict";
     
     console.log("documentCollectionTest.js");
-    console.log("will run for one minute");
+    
+    function createTestResource() {
+        var content = "Hello, world!";
+    
+        var metadata = {id: "test", contentType: "text/plain"};
+        
+        pointrel20141201Client.storeInNewEnvelope(content, metadata, function(error, result) {
+            if (error) { console.log("error", error); return;}
+            
+            console.log("Stored item", result.sha256AndLength, result);
+            
+            /*
+            pointrel20141201Client.fetchEnvelope(result.sha256AndLength, function(error, item) {
+                if (error) { console.log("error", error); return;}
+                console.log("Got item", item);
+            });
+            */
+        });
+    }
+    
+    function test() {
 
-    var documentCollection = new DocumentCollection();
+        var documentCollection = new DocumentCollection();
+        
+        documentCollection.startup();
+        
+        console.log("will run for one minute");
+        window.setTimeout(function() {
+            console.log("shutting down test after one minute");
+            documentCollection.shutdown();
+        }, 60000);
+    }
     
-    documentCollection.startup();
+    createTestResource();
+    test();
     
-    window.setTimeout(function() {
-        console.log("shutting down test after one minute");
-        documentCollection.shutdown();
-    }, 60000);
 });
