c537709a06f8ad769f9653032d2c6cba3f267d9c
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-22 10:04:19 -0400

    Fixed up PointrelCacheTest for previous file renaming Conflicts:
    	WebContent/testing/PointrelCacheTest.html
    	WebContent/testing/PointrelCacheTest.js

--- WebContent/testing/PointrelCacheTest.js
+++ WebContent/testing/PointrelCacheTest.js
@@ -1,48 +1,20 @@
 require([
-    "js/pointrel20141201Client",
-    "js/pointrel20141201DocumentCollection",
+    "js/PointrelCache",
     "dojo/domReady!"
 ], function(
-    pointrel20141201Client,
-    DocumentCollection
+    PointrelCache
 ){
     "use strict";
     
-    console.log("documentCollectionTest.js");
-    
-    function createTestResource() {
-        var content = "Hello, world!";
-    
-        var metadata = {id: "test", contentType: "text/plain"};
-        
-        pointrel20141201Client.storeInNewEnvelope(content, metadata, function(error, result) {
-            if (error) { console.log("error", error); return;}
-            
-            console.log("Stored item", result.sha256AndLength, result);
-            
-            /*
-            pointrel20141201Client.fetchEnvelope(result.sha256AndLength, function(error, item) {
-                if (error) { console.log("error", error); return;}
-                console.log("Got item", item);
-            });
-            */
-        });
-    }
-    
-    function test() {
+    console.log("PointrelCacheTest.js");
+    console.log("will run for one minute");
 
-        var documentCollection = new DocumentCollection();
-        
-        documentCollection.startup();
-        
-        console.log("will run for one minute");
-        window.setTimeout(function() {
-            console.log("shutting down test after one minute");
-            documentCollection.shutdown();
-        }, 60000);
-    }
+    var pointrelCache = new PointrelCache();
     
-    createTestResource();
-    test();
+    pointrelCache.startup();
     
+    window.setInterval(function() {
+        console.log("shutting down test after one minute");
+        pointrelCache.shutdown();
+    }, 60000);
 });
